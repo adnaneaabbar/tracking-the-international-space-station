dc4e82992ba5941bb5d7ec9ee2ce457883ef67bb
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-07-01 11:55:29 +0100

    lon and lat to pixels

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # get the location of ISS
-def get_space_station_location():
+def get_location():
 
     space_station_longitude = None
     space_station_latitude = None
@@ -24,3 +24,10 @@ def get_space_station_location():
     except:
         print('Request not working')
     return (space_station_longitude, space_station_latitude)
+
+def translate_to_pixels(longitude, latitude, max_x_px, max_y_px):
+    # y = -90 to 90 and x = -180 to 180
+    scale_x = abs(((longitude + 180) / 360) * max_x_px)
+    scale_y = abs(((latitude - 90) / 180) * max_y_px) #y scale is flipped
+
+    return scale_x, scale_y
